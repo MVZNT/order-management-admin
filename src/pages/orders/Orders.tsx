d1ed1e4196d35bd4bad5a87466ef7de290d752be
juratali <juratbekxudayberganov9@gmail.com>
2024-05-27 14:40:04 +0500

    some bugs fixed-2

--- src/pages/orders/Orders.tsx
+++ src/pages/orders/Orders.tsx
@@ -137,7 +137,7 @@ const Orders = () => {
 
                 <div className={"flex gap-2 max-lg:justify-end"}>
                     <h1 className={"text-sm self-center text-gray-400"}>Last
-                        sync: {dateFormatter(syncLogData?.syncedDate)} ({syncLogData?.spentTime})</h1>
+                        sync: {dateFormatter(syncLogData?.syncedDate || "")} ({syncLogData?.spentTime || ""})</h1>
 
                     <div
                         onClick={syncOrdersModal.onOpen}
