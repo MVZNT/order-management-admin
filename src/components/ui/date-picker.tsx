4be683aeba137399ddb8550a330e3a8765c008a4
Abdulbosit <abdulbosit1995@gmail.com>
2024-07-30 16:06:12 +0500

    Update date-picker.tsx
    1. The filter closes when clicking outside. 
    2. Fixing the date format

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -1,14 +1,13 @@
 "use client"
 
 import * as React from "react"
-import {CalendarIcon} from "@radix-ui/react-icons"
+import { CalendarIcon } from "@radix-ui/react-icons"
+import { cn } from "@/lib/utils"
+import { Calendar } from "@/components/ui/calendar"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { MdOutlineClear } from "react-icons/md";
 
-import {cn} from "@/lib/utils"
-import {Calendar} from "@/components/ui/calendar"
-import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover"
-import {MdOutlineClear} from "react-icons/md";
-
-const DatePickerDemo = ({placeholder, date, setDate, className}: {
+const DatePickerDemo = ({ placeholder, date, setDate, className }: {
     placeholder: string,
     date?: Date | undefined,
     setDate?: (date: Date | undefined) => void,
@@ -16,12 +15,27 @@ const DatePickerDemo = ({placeholder, date, setDate, className}: {
 }) => {
     const [isOpen, setIsOpen] = React.useState(false)
     const [isChoosingDate, setIsChoosingDate] = React.useState<boolean>(false)
+    const popoverRef = React.useRef<HTMLDivElement>(null)
 
     const onSelectDate = (date: Date | undefined) => {
         setIsOpen(false)
         setIsChoosingDate(false)
         setDate?.(date)
     }
+
+    React.useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (popoverRef.current && !popoverRef.current.contains(event.target as Node)) {
+                setIsOpen(false)
+                setIsChoosingDate(false)
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside)
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside)
+        }
+    }, [])
 
     return (
         <Popover
@@ -43,22 +57,22 @@ const DatePickerDemo = ({placeholder, date, setDate, className}: {
                         !date && "text-muted-foreground"
                     )}
                 >
-                    <CalendarIcon className="mr-2 h-4 w-4"/>
-                    {date ? <span>{date.toLocaleString().slice(0, 10)}</span> :
-                        <span>{placeholder || "Pick a date"}</span>}
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {date ? <span>{date.toLocaleDateString('en-US')}</span> : <span>{placeholder || "Pick a date"}</span>}
                 </div>
             </PopoverTrigger>
             <PopoverContent
+                ref={popoverRef}
                 className="flex flex-col gap-1"
             >
                 <div className={"flex justify-end"}>
                     <div className={"flex items-end text-orange-600 cursor-pointer gap-1"}
-                         onClick={() => {
-                             setIsOpen(!isOpen)
-                             setDate?.(undefined)
-                         }}>
+                        onClick={() => {
+                            setIsOpen(!isOpen)
+                            setDate?.(undefined)
+                        }}>
                         <span className={"text-sm "}>Clear date</span>
-                        <MdOutlineClear className={"text-base"}/>
+                        <MdOutlineClear className={"text-base"} />
                     </div>
                 </div>
                 <Calendar
